e5821b8b8c528cf8ac49853f590174824b68b997
walkerning <foxdoraame@gmail.com>
2016-10-17 23:57:31 +0800

    remove unneccesary files

--- src/gui_game/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "game.h"
-
-int main(int argc, char* argv[]) {
-  QApplication app(argc, argv);
-  GameGui* gui = new GameGui();
-  gui->show();
-  return app.exec();
-}
--- src/gui_test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "interface.h"
-
-int main(int argc, char* argv[]) {
-  QApplication app(argc, argv);
-  Gui* gui = new Gui();
-  gui->show();
-  return app.exec();
-}
